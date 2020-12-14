a90c0088b6e84505b56639a4ef04d219e0394ed0
vladlena <makogonvlada@gmail.com>
2020-12-14 13:50:48 +0200

    dz-6-corrections

--- script.js
+++ script.js
@@ -26,45 +26,49 @@ const questions = [
     },
 ];
 
-let answerArr = showQuestion();
+let answerArr = showQuestions();
 
 let points = calcPoints();
 let result = showResult(points);
 
-function showQuestion() {
+function showQuestions() {
     let questionsArr = questions.map(({ textQ }) => textQ);
     let typeArr = questions.map(({ type }) => type);
-    let massive = [];
-    massive = defQuestionType(questionsArr, typeArr);
-    return massive;
+    let answers = [];
+    answers = getAnswers(questionsArr, typeArr);
+    return answers;
 }
 
-// function mapMassive(massive, key) {
-//     return massive.map(({ key }) => key);
-// }
-
-
-function defQuestionType(qArr, tArr) {
-    let massive = [];
+function getAnswers(qArr, tArr) {
+    let answersMas = [];
     for (let i = 0; i < qArr.length; i++) {
         if (tArr[i] === 'confirmation') {
-            massive[i] = confirm(qArr[i]);
+            answersMas[i] = confirm(qArr[i]);
         } else {
-            massive[i] = prompt(qArr[i]);
+            answersMas[i] = prompt(qArr[i]);
         }
     }
-    return massive
+    return answersMas
 }
+
+// function getAnswers(qArr, tArr) {
+//     let answersMas = [];
+//     qArr.forEach (function(item, 0, qArr)) {
+//         if (tArr[i] === 'confirmation') {
+//             answersMas[i] = confirm(qArr[i]);
+//         } else {
+//             answersMas[i] = prompt(qArr[i]);
+//         }
+//     }
+//     return answersMas
+// }
 
 function calcPoints() {
     let correctAnswers = questions.map(({ answer }) => answer);
     let result = 0;
     for (let i = 0; i < answerArr.length; i++) {
-        if (answerArr[i] === correctAnswers[i]) {
-            result += 10;
-        } else { 
-            result += 0;
-        }
+        (answerArr[i] === correctAnswers[i]) ?
+            result += 10 : result += 0;
     }
     return result
 }
